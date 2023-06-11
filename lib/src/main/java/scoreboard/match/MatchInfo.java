ae91a5e5370ebf776571e7a3441044c140f9c155
Mateusz Frankowski <mateofrankowski@gmail.com>
2023-06-11 11:24:05 +0200

    Add sorted summary test case

--- lib/src/main/java/scoreboard/match/MatchInfo.java
+++ lib/src/main/java/scoreboard/match/MatchInfo.java
@@ -1,8 +1,10 @@
 package scoreboard.match;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class MatchInfo {
+
     private final MatchUp matchUp;
     private final Score score;
 
@@ -11,7 +13,24 @@ public class MatchInfo {
         this.score = scorboardEntry.getValue();
     }
 
+    public MatchUp getMatchUp() {
+        return matchUp;
+    }
+
     public Score getScore() {
         return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MatchInfo matchInfo = (MatchInfo) o;
+        return matchUp.equals(matchInfo.matchUp) && score.equals(matchInfo.score);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(matchUp, score);
     }
 }
--- lib/src/test/java/scoreboard/WorldCupScoreBoardTest.java
+++ lib/src/test/java/scoreboard/WorldCupScoreBoardTest.java
@@ -2,9 +2,10 @@ package scoreboard;
 
 import org.junit.jupiter.api.Test;
 import scoreboard.match.MatchInfo;
+import scoreboard.match.MatchUp;
+import scoreboard.match.Score;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -101,5 +102,33 @@ class WorldCupScoreBoardTest {
 
         assertEquals(matchInfo.size(), 0);
         assertEquals(result.getMessage(), "Cannot update score for non-existent match: Poland - Argentina");
+    }
+
+    @Test
+    void should_sort_matches_summary_by_total_score() {
+        // given
+        ScoreBoard scoreBoard = new WorldCupScoreBoard();
+
+        scoreBoard.startMatch("Poland", "Argentina");
+        scoreBoard.updateScore("Poland", "Argentina", 0, 1);
+
+        scoreBoard.startMatch("Mexico", "Sweden");
+        scoreBoard.updateScore("Mexico", "Sweden", 3, 2);
+
+        scoreBoard.startMatch("Spain", "Germany");
+        scoreBoard.updateScore("Spain", "Germany", 1, 1);
+
+        // when
+        List<MatchInfo> resultSummary = scoreBoard.getMatchesSummary();
+
+        // then
+        assertEquals(resultSummary.get(0).getMatchUp(), new MatchUp("Mexico", "Sweden"));
+        assertEquals(resultSummary.get(0).getScore(), new Score(3, 2));
+
+        assertEquals(resultSummary.get(1).getMatchUp(), new MatchUp("Spain", "Germany"));
+        assertEquals(resultSummary.get(1).getScore(), new Score(1, 1));
+
+        assertEquals(resultSummary.get(2).getMatchUp(), new MatchUp("Poland", "Argentina"));
+        assertEquals(resultSummary.get(2).getScore(), new Score(0, 1));
     }
 }
