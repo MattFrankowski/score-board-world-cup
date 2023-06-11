d7fad30fce1b77932c5267c6d81c7781e4ebf351
Mateusz Frankowski <mateofrankowski@gmail.com>
2023-06-11 09:55:43 +0200

    Implement start new match feature

--- lib/src/main/java/scoreboard/WorldCupScoreBoard.java
+++ lib/src/main/java/scoreboard/WorldCupScoreBoard.java
@@ -1,18 +1,28 @@
 package scoreboard;
 
 import scoreboard.match.MatchInfo;
+import scoreboard.match.MatchUp;
+import scoreboard.match.Score;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class WorldCupScoreBoard implements ScoreBoard {
 
+    private Map<MatchUp, Score> matches = new HashMap<>();
+
     @Override
     public void startMatch(String homeTeamName, String awayTeamName) {
-
+        MatchUp matchUp = new MatchUp();
+        matches.put(matchUp, new Score());
     }
 
     @Override
     public List<MatchInfo> getMatchesSummary() {
-        return List.of();
+        return matches.entrySet()
+                      .stream()
+                      .map(MatchInfo::new)
+                      .toList();
     }
 }
--- lib/src/main/java/scoreboard/match/MatchInfo.java
+++ lib/src/main/java/scoreboard/match/MatchInfo.java
@@ -1,4 +1,13 @@
 package scoreboard.match;
 
+import java.util.Map;
+
 public class MatchInfo {
+    private final MatchUp matchUp;
+    private final Score score;
+
+    public MatchInfo(Map.Entry<MatchUp, Score> scorboardEntry) {
+        this.matchUp = scorboardEntry.getKey();
+        this.score = scorboardEntry.getValue();
+    }
 }
